2a99eec6fadb285e3db202ea62df6c9fd4b6ab54
Dries-Fierens <dries.fierens@outlook.be>
2026-04-23 09:55:35 +0200

    Add fallback to the path of Astar

--- Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
@@ -1,4 +1,5 @@
 ﻿#include "AStar.h"
+#include <algorithm>
 #include <list>
 
 using namespace GameAI;
@@ -16,27 +17,33 @@ std::vector<Node*>AStar::FindPath(Node* const pStartNode, Node* const pGoalNode)
 	std::list<NodeRecord> closedList{};
 	NodeRecord currentNodeRecord{};
 	NodeRecord startNodeRecord{};
-	
+
 	startNodeRecord.pNode = pStartNode;
 	startNodeRecord.pConnection = nullptr;
 	startNodeRecord.costSoFar = 0;
 	startNodeRecord.estimatedTotalCost = GetHeuristicCost(pStartNode, pGoalNode);
 	openList.push_back(startNodeRecord);
-	
+
+	bool goalReached = false;
+
 	while (!openList.empty())
 	{
 		currentNodeRecord = *std::min_element(openList.begin(), openList.end());
-		
-		if (currentNodeRecord.pNode == pGoalNode) break;
-		
+
+		if (currentNodeRecord.pNode == pGoalNode)
+		{
+			goalReached = true;
+			break;
+		}
+
 		const auto& connections = pGraph->FindConnectionsFrom(currentNodeRecord.pNode->GetId());
 		for (const auto& connection : connections)
 		{
 			Node* pNextNode = pGraph->GetNode(connection->GetToId()).get();
 			float totalCostSoFar = currentNodeRecord.costSoFar + connection->GetWeight();
-			
+
 			auto closedNodeIt = std::find_if(closedList.begin(), closedList.end(), [pNextNode](const NodeRecord& record) { return record.pNode == pNextNode; });
-			
+
 			if (closedNodeIt != closedList.end() && closedNodeIt->costSoFar <= totalCostSoFar)
 			{
 				continue;
@@ -45,7 +52,7 @@ std::vector<Node*>AStar::FindPath(Node* const pStartNode, Node* const pGoalNode)
 			{
 				closedList.erase(closedNodeIt);
 			}
-			
+
 			auto openNodeIt = std::find_if(openList.begin(), openList.end(), [pNextNode](const NodeRecord& record) { return record.pNode == pNextNode; });
 
 			if (openNodeIt != openList.end() && openNodeIt->costSoFar <= totalCostSoFar)
@@ -56,30 +63,69 @@ std::vector<Node*>AStar::FindPath(Node* const pStartNode, Node* const pGoalNode)
 			{
 				openList.erase(openNodeIt);
 			}
-			
-			NodeRecord newRecord;
+
+			NodeRecord newRecord{};
 			newRecord.pNode = pNextNode;
 			newRecord.pConnection = connection;
 			newRecord.costSoFar = totalCostSoFar;
 			newRecord.estimatedTotalCost = totalCostSoFar + GetHeuristicCost(pNextNode, pGoalNode);
 			openList.push_back(newRecord);
 		}
-		
+
 		openList.remove(currentNodeRecord);
 		closedList.push_back(currentNodeRecord);
 	}
-	
+
+	// If A* never reaches pGoalNode
+	if (!goalReached)
+	{
+		auto fallbackNodeIt = std::min_element(closedList.begin(), closedList.end(),
+			[this, pGoalNode](const NodeRecord& lhs, const NodeRecord& rhs)
+			{
+				const float lhsHeuristic = GetHeuristicCost(lhs.pNode, pGoalNode);
+				const float rhsHeuristic = GetHeuristicCost(rhs.pNode, pGoalNode);
+
+				// If two nodes are equally close to the goal, it prefers the one with the lower costSoFar
+				if (lhsHeuristic == rhsHeuristic)
+				{
+					return lhs.costSoFar < rhs.costSoFar;
+				}
+
+				return lhsHeuristic < rhsHeuristic;
+			});
+
+		if (fallbackNodeIt == closedList.end())
+		{
+			return path;
+		}
+
+		// Set fallback node as destination
+		currentNodeRecord = *fallbackNodeIt;
+	}
+
 	// Backtracking
 	while (currentNodeRecord.pNode != pStartNode)
 	{
 		path.push_back(currentNodeRecord.pNode);
-		auto closedNodeIt = std::find_if(closedList.begin(), closedList.end(), [&currentNodeRecord](const NodeRecord& record) { return record.pNode->GetId() == currentNodeRecord.pConnection->GetFromId(); });
+
+		auto closedNodeIt = std::find_if(closedList.begin(), closedList.end(),
+			[&currentNodeRecord](const NodeRecord& record)
+			{
+				return record.pNode->GetId() == currentNodeRecord.pConnection->GetFromId();
+			});
+
+		if (closedNodeIt == closedList.end())
+		{
+			path.clear();
+			return path;
+		}
+
 		currentNodeRecord = *closedNodeIt;
 	}
 
 	path.push_back(pStartNode);
 	std::reverse(path.begin(), path.end());
-	
+
 	return path;
 }
 
